fdcfe6eaf358ee58e452d22ff44e3b696ed80a74
Alfred Mathew <alfredmathew718@gmail.com>
2021-07-15 20:07:19 -0400

    setup app server for CNC

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const nodemailer = require("nodemailer");
+
+// Constants
+const PORT = process.env.PORT || 8080;
+
+// App
+const app = express();
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+    let body = req.body;
+    let transporter = nodemailer.createTransport({
+        host: process.env.SMTP,
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: process.env.USER_NAME, 
+          pass: process.env.PASSWORD, 
+        },
+    });
+    let body_html = `\
+    <h1>${body.name}</h1>
+    <h2>${body.email}</h2>
+    <h3>${body.phone}</h3>
+    <p>${body.message}</p>`;
+    transporter.sendMail({
+        from: `"CNC App Server" <${process.env.USER_NAME}>`, // sender address
+        to: process.env.RECIPIENT, // list of receivers
+        subject: "Feedback - Called & Chosen", // Subject line
+        html: body_html
+    });
+    res.send("Thanks, we appreciate your response!");
+});
+
+app.listen(PORT);
+console.log(`Running!`);
